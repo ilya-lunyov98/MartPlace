93ae51718cc98e6e5831ab8d9a54a5d008022bda
ilya-lunyov98 <ilya.lunyov.98@gmail.com>
2020-04-22 22:21:50 +0300

    added sub-slier

--- app/js/main.js
+++ app/js/main.js
@@ -8,4 +8,17 @@ $(function(){
       });
 
       var mixer = mixitup('.mixer-mix');
+
+      $(document).ready(function(){
+        $('.slider-sub__items').slick({
+            appendArrows:$('.slider-sub-controls__buttons'),      
+            nextArrow:'<button type="button" class="slick-next">></button>',
+            prevArrow:'<button type="button" class="slick-prev"><</button>',
+            adaptiveHeight:true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite:true,
+            touchThreshold: 10,
+        });
+      });
 })
